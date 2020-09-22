d9c89c8e6c9127b6eb988a5dbffa83ccdf15ec14
bit1120172710 <35914028+bit1120172710@users.noreply.github.com>
2020-09-22 17:25:03 +0800

    完成control_unit

--- srcs/control_unit.v
+++ srcs/control_unit.v
@@ -18,80 +18,33 @@ module control_unit(
          output wire [`REG_DST_LENGTH]          regDst_muxD,
          output wire                            memToRegD // 是否为从内存加载到寄存器中的指令
        );
+    wire [15:0] controlCode;
+    assign controlCode = 
+    (opcode == `OP_ZEROS && func == `FUNC_ADDU) ?  16'b1000010000011001:
+    (opcode == `OP_ADDIU)? 16'b1000010001010101 :
+    (opcode == `OP_ZEROS && func == `FUNC_SUBU)? 16'b1000101100011001 :
+    (opcode == `OP_ZEROS && func == `FUNC_SLT)? 16'b1000110000011001 :
+    (opcode == `OP_SLTI) ? 16'b1000110001010101  :
+    (opcode == `OP_ZEROS && func == `FUNC_SLTU)? 16'b1000110100011001 :
+    (opcode == `OP_SLTIU)? 16'b1000110101010101 :
+    (opcode == `OP_LUI)?16'b1000111000011000  :
+    (opcode == `OP_ZEROS && func == `FUNC_AND)? 16'b1000011100010100 :
+    (opcode == `OP_ANDI)? 16'b1000011101010100 :
+    (opcode == `OP_ZEROS && func == `FUNC_NOR)? 16'b1000100100011000 :
+    (opcode == `OP_ZEROS && func == `FUNC_OR)? 16'b1000101000011000 :
+    (opcode == `OP_ORI)? 16'b1000101001010100 :
+    (opcode == `OP_ZEROS && func == `FUNC_XOR)? 16'b1000000100011000 :
+    (opcode == `OP_XORI)? 16'b1000000101010100:
+    16'h0000;
 
-
-// 算数运算指令
-    wire is_addu;
-    wire is_addiu;
-    wire is_subu;
-    wire is_slt;
-    wire is_slti;
-    wire is_sltu;
-    wire is_sltiu;
-    wire is_algor_instr;
-
-    assign is_addu = (opcode == `OP_ZEROS && func == `FUNC_ADDU);
-    assign is_addiu= (opcode == `OP_ADDIU);
-    assign is_subu = (opcode == `OP_ZEROS && func == `FUNC_SUBU);
-    assign is_slt  = (opcode == `OP_ZEROS && func == `FUNC_SLT);
-    assign is_slti = (opcode == `OP_SLTI);
-    assign is_sltu = (opcode == `OP_ZEROS && func == `FUNC_SLTU);
-    assign is_sltiu= (opcode == `OP_SLTIU);
-    assign is_algor_instr = (is_addu|| is_addiu||is_subu|| is_slt|| is_slti|| is_sltu|| is_sltiu);
-    
-    //逻辑运算指令
-    wire is_lui;
-    wire is_and;
-    wire is_andi;
-    wire is_nor;
-    wire is_or;
-    wire is_ori;
-    wire is_xor;
-    wire is_xori;
-    wire is_logic_instr;
-  
-    assign is_lui   = (opcode == `OP_LUI);
-    assign is_and   = (opcode == `OP_ZEROS && func == `FUNC_AND);
-    assign is_andi  = (opcode == `OP_ANDI);
-    assign is_nor   = (opcode == `OP_ZEROS && func == `FUNC_NOR);
-    assign is_or    = (opcode == `OP_ZEROS && func == `FUNC_OR);                                        
-    assign is_ori   = (opcode == `OP_ORI);
-    assign is_xor   = (opcode == `OP_ZEROS && func == `FUNC_XOR);
-    assign is_xori  = (opcode == `OP_XORI);
-    assign is_logic_instr = (is_xor || is_lui || is_and || is_andi || is_nor||is_or || is_ori ||is_xor || is_xori);
-    
-    //移位指令
-    wire is_sllv;
-    wire is_sll;
-    wire is_srav;
-    wire is_sra;
-    wire is_srlv;
-    wire is_srl;
-    wire is_shift_instr;
-
-    assign is_sllv  = (opcode == `OP_ZEROS && func == `FUNC_SLLV);
-    assign is_sll  = (opcode == `OP_ZEROS && func == `FUNC_SLL);
-    assign is_srav  = (opcode == `OP_ZEROS && func == `FUNC_SRAV);
-    assign is_sra  = (opcode == `OP_ZEROS && func == `FUNC_SRA);
-    assign is_srlv  = (opcode == `OP_ZEROS && func == `FUNC_SRLV);
-    assign is_srl  = (opcode == `OP_ZEROS && func == `FUNC_SRL);
-    assign is_shift_instr = (is_sllv || is_sll || is_srav || is_sra || is_srlv || is_srl);
- 
-    //分支跳转指令
-    wire is_beq;
-    wire is_bne;
-    wire is_j;
-
-    assign is_beq   = (opcode == `OP_BEQ);
-    assign is_bne   = (opcode == `OP_BNE);
-    assign is_j     = (opcode == `OP_J);
-
-    //访存指令
-    wire is_lw;
-    wire is_sw;
-    assign is_sw    = (opcode == `OP_SW);
-    assign is_lw    = (opcode == `OP_LW);
-
-    assign memToRegD=is_lw;
-
+    assign Regfile_weD=controlCode[15];
+    assign DataMem_weD=controlCode[14];
+    assign npcOp=controlCode[13:12];
+    assign aluOpD=controlCode[11:8];
+    assign aluSrc1_muxD=controlCode[7];
+    assign aluSrc2_muxD=controlCode[6];
+    assign regSrc_muxD=controlCode[5:4];
+    assign regDst_muxD=controlCode[3:2];
+    assign memToRegD=controlCode[1];
+    assign extOpD=controlCode[0];
 endmodule
